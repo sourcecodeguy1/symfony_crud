143e688c4cb5cbe4bdd2f652b9b1b64ef4c3f5e5
Julio Sandoval <sandovaljulio2@yahoo.com>
2021-09-18 03:26:36 -0700

    added specific selected id from delete btn

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,12 @@ const posts = document.getElementById("posts");
 
 if(posts){
     posts.addEventListener('click',  e => {
+
        if(e.target.className === 'btn btn-danger delete-post'){
-           if(confirm('Are you sure you want to delete this record?')){
-               const id = e.target.getAttribute('data-id');
+
+           const id = e.target.getAttribute('data-id');
+
+           if(confirm(`Are you sure you want to delete this record with id ${id}?`)){
 
                fetch(`/post/delete/${id}`,{
                    method: 'DELETE'
